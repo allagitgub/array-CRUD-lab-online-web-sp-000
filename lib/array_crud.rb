05f30583cb3ee66afe05b89e782e459cac8c3349
Alla Kuzminova <alchik@gmail.com>
2020-09-07 01:28:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-
+  elements = ["one", "two", "three", "four"]
+  elements << "five"
 end
 
 def add_element_to_start_of_array(array, element)
